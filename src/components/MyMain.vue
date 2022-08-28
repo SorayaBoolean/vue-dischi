4b3bdbf9176394f9e00eae9cc52e2ffadf7bb839
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-08-28 18:05:06 +0200

    all structure is well linked and also the axios data and the son. props were inserted and data were written in template but does not appear

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,30 +1,19 @@
 <template>
-  <div >
-    <div class="container">
-      <div class="discs_container">
-          <div class="singleDisc">
-            <div class="img_card">
-              img
-            </div>
-            <div class="title_card">
-              title
-            </div>
-            <div class="author_card">
-              author
-            </div>
-          </div>    
-      </div>
-      
-    </div>
-  </div>
+ <div>
+  <SingleDisc v-for="(disc, index) in MyMain" :key="index" :disc="disc"/>
+ </div>
 </template>
 
 <script>
 
 import axios from 'axios'
+import SingleDisc from '@/components/SingleDisc.vue'
 
 export default {
     name: 'MyMain',
+    components: {
+    SingleDisc
+},
     data () {
       return {
         MyMain: [],
@@ -45,58 +34,12 @@ export default {
         }
       }
     }
+
+    
 </script>
 
 
 <style scoped lang="scss">
-  .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height:700px;
-      background-color: #1E2D3B;
-  }
-
-  .discs_container{
-      display:flex;
-      flex-wrap:wrap;
-      justify-content: center;
-      width:1000px;
-      height:600px;
-  }
-
-  .singleDisc {
-    height: calc(100% / 2);
-    width: calc(100% /6);
-    margin: 10px;
-    border:1px solid white;
-    background-color:#2E3A46;
-
-      .img_card {
-        margin: auto;
-        margin-top:15px;
-        height: 130px;
-        width: 130px;
-        border: 1px solid green;
-
-      }
-      .title_card{
-        margin-top:15px;
-        padding: 15px;
-        text-align: center;
-        color: white;
-        border: 1px solid lightblue;
-      }
-      .author_card {
-        margin-top:15px;
-        padding: 15px;
-        text-align: center;
-        color: grey;
-        border: 1px solid orange;
-      }
-    
-
-  }
-
+  
 
 </style>
---
+++ src/components/SingleDisc.vue
@@ -0,0 +1,80 @@
+<template>
+   <div class="container">
+      <div class="discs_container">
+          <div class="singleDisc">
+            <div class="img_card">
+              <img :src="disc.poster" alt="poster">
+            </div>
+            <div class="title_card">
+              <h4>{{disc.title}}</h4>
+            </div>
+            <div class="author_card">
+              <p>{{disc.author}}</p>
+              <p>{{disc.year}}</p>
+            </div>
+          </div>    
+      </div>
+      
+    </div>
+</template>
+
+<script>
+export default {
+name: 'SingleDisc',
+props: {
+    disc: Object
+}
+}
+</script>
+
+<style scoped lang="scss">
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height:700px;
+      background-color: #1E2D3B;
+  }
+
+  .discs_container{
+      display:flex;
+      flex-wrap:wrap;
+      justify-content: center;
+      width:1000px;
+      height:600px;
+  }
+
+  .singleDisc {
+    height: calc(100% / 2);
+    width: calc(100% /6);
+    margin: 10px;
+    border:1px solid white;
+    background-color:#2E3A46;
+
+      .img_card {
+        margin: auto;
+        margin-top:15px;
+        height: 130px;
+        width: 130px;
+        border: 1px solid green;
+
+      }
+      .title_card{
+        margin-top:15px;
+        padding: 15px;
+        text-align: center;
+        color: white;
+        border: 1px solid lightblue;
+      }
+      .author_card {
+        margin-top:15px;
+        padding: 15px;
+        text-align: center;
+        color: grey;
+        border: 1px solid orange;
+      }
+    
+
+  }
+
+</style>
