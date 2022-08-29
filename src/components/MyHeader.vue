6c2e019673c626f719f2311073d5a27f6541dde1
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-08-29 17:21:17 +0200

    add added a select in header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -3,6 +3,13 @@
 <div class="container"> 
    <nav>
     <img src="@/img/Spotify_logo_without_text.svg.webp" alt="logo">
+
+    <div class="select_container">
+        <select name="genere" id="genere">
+            <option value="opt1">Seleziona un genere</option>
+        </select>
+    </div>
+    
    </nav>
 </div>
   
@@ -18,6 +25,9 @@ export default {
 <style scoped lang="scss">
 
 nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
     height: 50px;
     background-color: #2E3A46;
@@ -26,6 +36,14 @@ nav {
             margin: 5px 5px;
             height: 40px;
             width: 40px;
+        }
+
+        .select_container{
+            margin-right: 10px;
+
+            select{
+                width: 150px;
+            }
         }
 }
 
---
+++ src/components/MySelect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <select name="Genre" id="genere">
+        <option value="opt1">Rock</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MySelect'
+}
+</script>
+
+<style>
+
+</style>
